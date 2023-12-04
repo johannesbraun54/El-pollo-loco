22fb55ead8a35e93c9bbb525d0a1e214bcebdb20
johannesbraun54 <johannesbraun02@gmail.com>
2023-12-04 15:25:42 +0100

    charcter running right and left, keyboard object

--- models/world.class.js
+++ models/world.class.js
@@ -11,17 +11,22 @@ class World{
         new BackgroundObject('img/5_background/layers/air.png', 0, 80),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0, 80),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0, 80),
-        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 80),
-        
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0, 80),   
     ]
     canvas;
     ctx;
-    newXPosition;
+    keyboard;
 
-    constructor(canvas){
+    constructor(canvas,keyboard){
         this.ctx = canvas.getContext('2d');
         this.canvas = canvas;
+        this.keyboard = keyboard
         this.draw();
+        this.setWorld();
+    }
+
+    setWorld(){
+        this.character.world = this
     }
     
     draw(){
