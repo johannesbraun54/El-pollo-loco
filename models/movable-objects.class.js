79cde5f2e5dacfa57c954ca0cd7df369dff4562e
johannesbraun54 <johannesbraun02@gmail.com>
2023-12-05 16:54:07 +0100

    add  isColliing

--- models/movable-objects.class.js
+++ models/movable-objects.class.js
@@ -74,5 +74,20 @@ class MovableObject {
         this.speedY = 30;
     }
 
+    /*isColliding(obj){
+        return  (this.X + this.width) >= obj.X && this.X <= (obj.X + obj.width) && 
+                (this.Y + this.offsetY + this.height) >= obj.Y &&
+                (this.Y + this.offsetY) <= (obj.Y + obj.height) //&& 
+                //obj.onCollisionCourse; // Optional: hiermit könnten wir schauen, ob ein Objekt sich in die richtige Richtung bewegt. Nur dann kollidieren wir. Nützlich bei Gegenständen, auf denen man stehen kann.
+    }*/
+
+    isColliding(mo){
+        return this.x + this.width > mo.x &&
+                this.y + this.height > mo.y &&
+                this.x < mo.x && this.y < mo.y + mo.height
+    }
+
+    
+
 
 }
--- models/world.class.js
+++ models/world.class.js
@@ -14,10 +14,21 @@ class World{
         this.keyboard = keyboard
         this.draw();
         this.setWorld();
+        this.checkCollisions();
     }
 
     setWorld(){
         this.character.world = this
+    }
+
+    checkCollisions(){
+        setInterval(() => {
+            this.level.enemies.forEach(enemy => {
+                if(this.character.isColliding(enemy)){
+                    console.log('collision with character', enemy)
+                }
+            });
+        }, 1000);
     }
     
     draw(){
